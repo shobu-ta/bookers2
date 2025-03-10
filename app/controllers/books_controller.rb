7414a6d87d4f1dbc453e35d5c29a88cf701e7cba
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-03-10 15:05:56 +0000

    make books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_url, notice: 'Book was successfully destroyed.'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+end
