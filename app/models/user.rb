b16f0ae901a718229e484a48cc431e6d569ba46b
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-03-16 02:16:18 +0000

    users modelchange

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  def profile_image_url
-    if profile_image.attached?
-      Rails.application.routes.url_helpers.url_for(profile_image)
-    else
-      'assets/images/noImage.jpg'
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/nolmage.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
 end
